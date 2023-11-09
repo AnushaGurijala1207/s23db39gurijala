0d1fd001d08874d6281a1251bb061750c749a364
AnushaGurijala1207 <136043406+AnushaGurijala1207@users.noreply.github.com>
2023-11-09 15:35:24 -0600

    made changes at the local repo

--- controllers/instrument.js
+++ controllers/instrument.js
@@ -1,54 +1,65 @@
 var Instrument = require('../models/instrument');
 // List of all Costumes
-exports.instrument_list = async function(req, res) {
-try{
-theInstruments = await Instrument.find();
-res.send(theInstruments);
-}
-catch(err){
-res.status(500);
-res.send(`{"error": ${err}}`);
-}
+exports.instrument_list = async function (req, res) {
+    try {
+        theInstruments = await Instrument.find();
+        res.send(theInstruments);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
 // VIEWS
 // Handle a show all view
-exports.instrument_view_all_Page = async function(req, res) {
-try{
-theInstruments = await Instrument.find();
-res.render('instruments', { title: 'Instrument Search Results', results: theInstruments });
+exports.instrument_view_all_Page = async function (req, res) {
+    try {
+        theInstruments = await Instrument.find();
+        res.render('instruments', { title: 'Instrument Search Results', results: theInstruments });
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 }
-catch(err){
-res.status(500);
-res.send(`{"error": ${err}}`);
-}
-}
-// for a specific Instruments.
-exports.instrument_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: Instrument detail: ' + req.params.id);
+
+// for a specific Costume.
+exports.instrument_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        let result = await Instrument.findById(req.params.id);
+
+        if (!result) {
+            res.status(404).send(`{"error": document for id ${req.params.id} not found`);
+        } else {
+            res.send(result);
+        }
+    } catch (error) {
+        res.status(500).send(`{"error": ${error.message}`);
+    }
 };
 // Handle Instrument create on POST.
-
-exports.instrument_create_post = async function(req, res) {
+exports.instrument_create_post = async function (req, res) {
     console.log(req.body)
-let document = new Instrument();
-document.instrument_name = req.body.instrument_name;
-document.manufacturer = req.body.manufacturer;
-document.price = req.body.price;
-try{
-    let result = await document.save();
-    res.send(result);
- }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    let document = new Instrument();
+    document.instrument_name = req.body.instrument_name;
+    document.manufacturer = req.body.manufacturer;
+    document.price = req.body.price;
+    try {
+        let result = await document.save();
+        res.send(result);
     }
- };
-    
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+
 // Handle Instrument delete form on DELETE.
-exports.instrument_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: Instrument delete DELETE ' + req.params.id);
+exports.instrument_delete = function (req, res) {
+    res.send('NOT IMPLEMENTED: Instrument delete DELETE ' + req.params.id);
 };
 // Handle Instrument update form on PUT.
-exports.instrument_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: Instrument update PUT' + req.params.id);
+exports.instrument_update_put = function (req, res) {
+    res.send('NOT IMPLEMENTED: Instrument update PUT' + req.params.id);
 };
--- routes/instruments.js
+++ routes/instruments.js
@@ -4,6 +4,7 @@ var router = express.Router();
 
 // GET Instruments
 router.get('/', instrument_controlers.instrument_view_all_Page );
- 
+ //GET request for one instrument.
+router.get('/instruments/:id', instrument_controlers.instrument_detail)
 
 module.exports = router;
