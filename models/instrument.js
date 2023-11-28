3dbacbf8adbf689e4ff11022037553fa10879a65
AnushaGurijala1207 <136043406+AnushaGurijala1207@users.noreply.github.com>
2023-11-28 14:45:00 -0600

    added functionality for detail and detail buttons providing security at the local repo

--- models/instrument.js
+++ models/instrument.js
@@ -8,11 +8,27 @@ router.get('/', function(req, res, next) {
 
 module.exports = router;
 
-
 const mongoose = require("mongoose")
 const instrumentSchema = mongoose.Schema({
-instrument_name: String,
-manufacturer: String,
-price: Number
-})
+  instrument_name: {
+    type: String,
+    required: true,
+  },
+  manufacturer: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (value) {
+        // Example validation: Check if the manufacturer contains only alphabetical characters
+        return /^[a-zA-Z]+$/.test(value);
+      },
+    },
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: [0, 'Price must be at least 0'],   // Minimum value for the price
+    max: [3000, 'Price must be at most 100'], // Maximum value for the price
+  },
+});
 module.exports = mongoose.model("Instrument",instrumentSchema)
